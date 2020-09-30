a3df395b246ccbccb417e084d1f7a52e10ac5668
HussainSakariya <sakariyahussain@gmail.com>
2020-09-30 11:46:55 +0530

    Create Student Model

--- studentApp/models.py
+++ studentApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=20)
+    image=models.ImageField(upload_to="student")
+    std=models.IntegerField()
+    address=models.TextField(max_length=50)
+    mo_number=models.IntegerField()
